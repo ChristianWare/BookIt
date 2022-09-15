8c1f2559abbda821e63896dd8876713810c919eb
Chris Ware <c.p.ware@hotmail.com>
2022-09-15 10:35:55 -0700

    Connecting to MongoDB

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  env: {
+    DB_LOCAL_URI:
+      "mongodb+srv://Admin:Password123@cluster0.xydd2ju.mongodb.net/?retryWrites=true&w=majority",
+  },
+};
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,9 +1,12 @@
 import nc from "next-connect";
+import dbConnect from "../../../config/dbConnect";
 
 import { allRooms } from "../../../controllers/roomController";
 
 const handler = nc();
 
-handler.get(allRooms)
+dbConnect();
+
+handler.get(allRooms);
 
 export default handler;

f297a87a44f47ed7dbaddf04ede1b0827b73965f
Chris Ware <c.p.ware@hotmail.com>
2022-09-15 16:56:42 -0700

    Validation & Mongoose ID Error

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -7,6 +7,18 @@ export default (err, req, res, next) => {
 
   error.message = err.message;
 
+  // Wrong mongoose ObjectID Error:
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    error = new ErrorHandler(message, 400);
+  }
+
+  // Handling mongoose validation error:
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     sucess: false,
     error,

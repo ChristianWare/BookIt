7f2f1300d568d30dba8ee1d2454069bf8a679067
Chris Ware <c.p.ware@hotmail.com>
2022-09-15 22:20:47 -0700

    Header and Footer

---
+++ comps/layout/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer class='py-1'>
+      <p class='text-center mt-1'>Book IT - 2019-2022, All Rights Reserved</p>
+    </footer>
+  );
+}
+export default Footer
